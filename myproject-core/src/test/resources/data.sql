5b16c9f08b78b360d329952dd37bbd37ace5685a
Andrés Bustamante <afbustag@gmail.com>
2025-03-29 20:57:07 +0100

    Fixed test datasets

--- myproject-core/src/test/resources/data.sql
+++ myproject-core/src/test/resources/data.sql
@@ -1,19 +1,20 @@
-INSERT INTO language (language_id, name, last_update) VALUES (1, 'English', now());
+INSERT INTO language (language_id, name, created_at, created_by, last_update, modified_by)
+VALUES (1, 'English', now(), 'Test', now(), 'Test');
 
-INSERT INTO category (category_id, name, last_update)
-VALUES (1, 'Action', '2006-02-15 04:46:27'),
-       (2, 'Animation', '2006-02-15 04:46:27'),
-       (3, 'Children', '2006-02-15 04:46:27'),
-       (4, 'Classics', '2006-02-15 04:46:27'),
-       (5, 'Comedy', '2006-02-15 04:46:27'),
-       (6, 'Documentary', '2006-02-15 04:46:27'),
-       (7, 'Drama', '2006-02-15 04:46:27'),
-       (8, 'Family', '2006-02-15 04:46:27'),
-       (9, 'Foreign', '2006-02-15 04:46:27'),
-       (10, 'Games', '2006-02-15 04:46:27'),
-       (11, 'Horror', '2006-02-15 04:46:27'),
-       (12, 'Music', '2006-02-15 04:46:27'),
-       (13, 'New', '2006-02-15 04:46:27'),
-       (14, 'Sci-Fi', '2006-02-15 04:46:27'),
-       (15, 'Sports', '2006-02-15 04:46:27'),
-       (16, 'Travel', '2006-02-15 04:46:27');
+INSERT INTO category (category_id, name, created_at, created_by, last_update, modified_by)
+VALUES (1, 'Action', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (2, 'Animation', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (3, 'Children', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (4, 'Classics', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (5, 'Comedy', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (6, 'Documentary', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (7, 'Drama', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (8, 'Family', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (9, 'Foreign', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (10, 'Games', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (11, 'Horror', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (12, 'Music', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (13, 'New', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (14, 'Sci-Fi', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (15, 'Sports', '2006-02-15 04:46:27', 'Test', now(), 'Test'),
+       (16, 'Travel', '2006-02-15 04:46:27', 'Test', now(), 'Test');
